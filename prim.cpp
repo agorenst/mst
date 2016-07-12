178e4136a4ee54e3ddfe1fee5e528e2d285f4232
agorenst <agorenst@cs.wisc.edu>
2016-07-12 09:14:38 -0400

    Finally made a real heap thing, but still some trickiness to work out (do we really need to remove all the node edges? Do we really need to reconstruct the entire heap?). Maybe I'll revisit this one day.

--- prim.cpp
+++ prim.cpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <array>
 
+using namespace std;
+
 struct edge {
     int u = -1;
     int v = -1;
@@ -15,9 +17,9 @@ struct edge {
     edge(int u, int v, int w): u(u), v(v), w(w) {}
 };
 
-using namespace std;
 
 const int infinity = numeric_limits<int>::max();
+
 
 int delete_min(list<int>& priority_queue, const vector<int>& costs) {
     auto min_elt_ptr = min_element(begin(priority_queue), end(priority_queue),
@@ -92,29 +94,41 @@ int main(int argc, char* argv[]) {
     }
 
     vector<int> costs(node_count,infinity);
-    list<int> priority_queue;
-    for (int i = 1; i < node_count; ++i) { priority_queue.push_back(i); }
+    costs[1] = 0;
+    auto cost_comparer = [&costs](int x, int y) { return costs[x] > costs[y]; };
+
+    vector<int> pq(node_count);
+
+    // lots of gnarly off-by-one errors potentially here, but the test cases seem OK.
+    for (int i = 0; i < node_count; ++i) { pq[i] = i+1; }
+    pq.pop_back();
     vector<int> prev(node_count,-1);
 
-    costs[1] = 0;
+    make_heap(begin(pq), end(pq), cost_comparer);
 
 
     // the current "stop" condition is a hack about how min_element works when
     // all the elements
-    for (int min_node = delete_min(priority_queue, costs);
-             min_node != 0;
-             min_node = delete_min(priority_queue, costs)) {
-        //cout << "min node: " << min_node << endl;
+    while (pq.size()) {
+        auto min_node = pq[0];
+        pop_heap(begin(pq), end(pq), cost_comparer);
+        pq.pop_back();
+
         for (auto&& v : g.neighbors(min_node)) {
-            //cout << "\tneighbor: " << v << endl;
             if (costs[v] > g.edge_weight(min_node,v)) {
                 costs[v] = g.edge_weight(min_node,v);
                 prev[v] = min_node;
-                decrease_key(priority_queue,costs,v);
+                // decrease key here
             }
         }
+        // can we be smarter about maintaining the priority queue?
+        make_heap(begin(pq), end(pq), cost_comparer);
+
+        // this doesn't remove the edge *weights* (weirdly),
+        // so we can reconstitute the graph easily
         g.remove_node(min_node);
     }
+
     for (int i = 2; i < prev.size(); ++i) {
         auto u = i;
         auto v = prev[i];
